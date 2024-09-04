7b628367b92494c077cd48c426481e13ee9abfd6
Asya Tealdi <asya2698@live.it>
2024-09-04 14:10:03 +0200

    Initial Commit

---
+++ PomodoroTimer/PomodoroTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTimerApp.swift
+//  PomodoroTimer
+//
+//  Created by Asya Tealdi on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
